335e756a3d2ec0ca4171fd83ce0dc6b52c19fc89
codeproj01 <cliftonmpls@comcast.net>
2019-09-27 14:16:32 -0500

    updated the apiRoutes.js with movies api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,22 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
+  // Get all movies
+  app.get("/api/movies", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
+  // Create a new movie
+  app.post("/api/movies", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
+  // Delete an movie by id
+  app.delete("/api/movies/:id", function(req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
