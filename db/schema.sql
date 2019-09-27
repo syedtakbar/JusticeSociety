8a8f7608df25218923b8dee400cb8693a13932c1
codeproj01 <cliftonmpls@comcast.net>
2019-09-27 13:35:19 -0500

    added a db directory and updated config.json

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,8 @@ CREATE TABLE movies (
     movie_id INT NOT NULL AUTO_INCREMENT,
     movie_title VARCHAR(45) NULL,
     actors_name VARCHAR (45) NULL,
+    director VARCHAR (45) NULL,
+    producer VARCHAR (45) NULL,
     genre VARCHAR (255) NULL,
     release_date DECIMAL (10) NULL,
     budget DECIMAL (10) NULL,
@@ -15,7 +17,7 @@ CREATE TABLE movies (
     original_language VARCHAR (255) NULL,
     runtime DECIMAL (10) NULL,
     country VARCHAR (45) NULL,
-    Rotten_tomatoes VARCHAR (255) NULL,
+    rotten_tomatoes VARCHAR (255) NULL,
     plot VARCHAR (255) NULL,
     ratings INT NOT NULL,
     PRIMARY KEY (movie_id)
--- db/seed.sql
+++ db/seed.sql
@@ -1,2 +1,3 @@
 -- INSERT INTO DEPARTMENT --
-INSERT INTO departments (movie_id, movie_title, actors_name, genre, release_date, budget, revenue, original_language, runtime, country, rotten_tomatoes, plot, ratings) 
+INSERT INTO movies (movie_id, movie_title, actors_name, director, producer, genre, release_date, budget, revenue, original_language, runtime, country, rotten_tomatoes, plot, ratings) 
+-- Value
