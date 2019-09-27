f8ef6130ca0fd3f473d912568acfe0df06a16a0d
codeproj01 <cliftonmpls@comcast.net>
2019-09-27 13:25:48 -0500

    added a db directory and updated config.json

---
+++ db/seed.sql
@@ -0,0 +1,2 @@
+-- INSERT INTO DEPARTMENT --
+INSERT INTO departments (movie_id, movie_title, actors_name, genre, release_date, budget, revenue, original_language, runtime, country, rotten_tomatoes, plot, ratings) 
