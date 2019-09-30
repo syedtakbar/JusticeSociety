ed7b0ac922fa587ea3930e133ecae19dff1c484a
Syed T. Akbar <sakbar01@icloud.com>
2019-09-30 10:55:10 -0500

    initial checkin

---
+++ models/review.js
@@ -0,0 +1,31 @@
+module.exports = function(sequelize, DataTypes) {
+  const Review = sequelize.define("Review", {
+    watch_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    reivew_body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    user_rating: {
+      type: DataTypes.DECIMAL(12, 2),
+      allowNull: false
+    }
+  });
+
+  Review.associate = function(models) {
+    Review.belongsTo(models.User, {
+      foreignKey: "user_id"
+    });
+  };
+
+  Review.associate = function(models) {
+    Review.belongsTo(models.Movie, {
+      foreignKey: "movie_id"
+    });
+  };
+
+  return Review;
+};
---
+++ models/user.js
@@ -0,0 +1,65 @@
+const bcrypt = require("bcrypt");
+
+var uuidv1 = require("uuid/v1");
+
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    uuid: {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      isUnique: true
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 30]
+      }
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 30]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 100]
+      }
+    },
+    user_key: {
+      type: DataTypes.STRING,
+      required: true,
+      validate: {
+        len: [8]
+      }
+    }
+  });
+
+  User.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  };
+
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.user_key);
+  };
+
+  User.associate = function(models) {
+    User.hasMany(models.Movie, {
+      foreignKey: "user_id",
+      onDelete: "cascade"
+    });
+  };
+
+  User.associate = function(models) {
+    User.hasMany(models.Review, {
+      foreignKey: "reviwer_id"
+    });
+  };
+
+  return User;
+};
